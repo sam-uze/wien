588233b320601ea7700515dc7a4ddabb97f2cf04
Samuel Uzelino <uzelino.sam@gmail.com>
2025-04-03 17:22:55 +0200

    Add functions to load and display various sightseeing routes and pedestrian zones in Vienna

--- main.js
+++ main.js
@@ -37,3 +37,39 @@ async function loadSights(url) {
 }
 loadSights("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SEHENSWUERDIGOGD&srsName=EPSG:4326&outputFormat=json");
 
+//sehnswürdigkeiten Touristische Kraftfahrlinien Liniennetz Vienna Sightseeing Linie Wien 
+async function loadSights(url) {
+    //console.log(url);
+    let response = await fetch(url);
+    let jasondata = await response.json();
+    //console.log(jasondata);
+    L.geoJSON(jasondata,{
+        attribution: "Datenquelle: <a href='https://data.wien.gv.at'>Stadt Wien</a>"
+    }).addTo(map);
+}
+loadSights("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKLINIEVSLOGD&srsName=EPSG:4326&outputFormat=json");
+
+//sehnswürdigkeiten Touristische Kraftfahrlinien Haltestellen Vienna Sightseeing Linie Standorte Wien
+async function loadSights(url) {
+    //console.log(url);
+    let response = await fetch(url);
+    let jasondata = await response.json();
+    //console.log(jasondata);
+    L.geoJSON(jasondata,{
+        attribution: "Datenquelle: <a href='https://data.wien.gv.at'>Stadt Wien</a>"
+    }).addTo(map);
+}
+loadSights("hhttps://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKHTSVSLOGD&srsName=EPSG:4326&outputFormat=json");
+
+//sehnswürdigkeiten Fußgängerzone
+async function loadSights(url) {
+    //console.log(url);
+    let response = await fetch(url);
+    let jasondata = await response.json();
+    //console.log(jasondata);
+    L.geoJSON(jasondata,{
+        attribution: "Datenquelle: <a href='https://data.wien.gv.at'>Stadt Wien</a>"
+    }).addTo(map);
+}
+loadSights("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:FUSSGEHERZONEOGD&srsName=EPSG:4326&outputFormat=json");
+
