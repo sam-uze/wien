554c87f9e6eac70ce239151c6db287fc7eb8e3f4
Samuel Uzelino <uzelino.sam@gmail.com>
2025-04-03 17:53:47 +0200

    Add overlay feature groups for sights, lines, stops, and zones to the map

--- main.js
+++ main.js
@@ -11,9 +11,13 @@ let stephansdom = {
 // Karte initialisieren
 let map = L.map("map").setView([stephansdom.lat, stephansdom.lng], stephansdom.zoom);
 
-// Hintergrundkarte definieren
-
-
+//Overlays definieren
+let overlays = {
+    "sights": L.featureGroup().addTo(map),
+    "lines": L.featureGroup().addTo(map),
+    "stops": L.featureGroup().addTo(map),
+    "zones": L.featureGroup().addTo(map),
+};
 //Layercontrol
 L.control.layers({
     "BasemapAT grau": L.tileLayer('https://mapsneu.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png', {
