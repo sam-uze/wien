5150a46aeb61a768081ce08fd50bb11e11e092ec
Samuel Uzelino <uzelino.sam@gmail.com>
2025-05-05 12:07:35 +0200

    Comment out console log in loadSights function and add detailed popup for loadLines function

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ async function loadSights(url) {
             });
         },
         onEachFeature: function (feature, layer) {
-            console.log(feature.properties);
+            //console.log(feature.properties);
             layer.bindPopup(`
                 <img src="${feature.properties.THUMBNAIL}" alt="*">
                 <h4>${feature.properties.NAME}</h4>
@@ -104,6 +104,15 @@ async function loadLines(url) {
                 color: lineColor
 
             }
+        },
+        onEachFeature: function (feature, layer) {
+            console.log(feature.properties);
+            layer.bindPopup(`
+                <h4><i class="fa-solid fa-bus"></i> ${feature.properties.LINE_NAME}</h4>
+                <p><i class="fa-regular fa-circle-stop"></i> ${feature.properties.FROM_NAME}<br>
+                <i class="fa-solid fa-arrow-down"></i> <br>
+                <i class="fa-regular fa-circle-stop"></i> ${feature.properties.TO_NAME}</p>
+            `);
         }
     }).addTo(overlays.lines);
 }
